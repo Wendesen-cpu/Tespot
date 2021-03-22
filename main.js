e952835948e02057c5cef70842056f497117f053
fithi2021 <lgfetah@gmail.com>
2021-03-22 20:10:31 +0100

    Nothing

--- main.js
+++ main.js
@@ -34,17 +34,10 @@ function setupMap(center){
 
 }
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("{{https://covid-19-testing.github.io/locations/:state/complete.json", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-
+fetch('https://covid-19-testing.github.io/locations/:state/complete.json')
+   
+  .then(response => response.json())
+  .then(data => console.log(JSON.parse(data)));
 
 
 
